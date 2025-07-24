54d0c823b05d7f7e37c240a4b6b5e3b3457a89c4
alexx-ftw <eroszansan@gmail.com>
2025-07-24 19:23:48 +0100

    docs: fix GitHub npx command

--- dist/tools/fetch-chunk.tool.d.ts
+++
@@ -1,3 +0,0 @@
-import { UnifiedTool } from './registry.js';
-export declare const fetchChunkTool: UnifiedTool;
-//# sourceMappingURL=fetch-chunk.tool.d.ts.map
--- dist/utils/commandExecutor.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function executeCommand(command: string, args: string[], onProgress?: (newOutput: string) => void): Promise<string>;
-//# sourceMappingURL=commandExecutor.d.ts.map
